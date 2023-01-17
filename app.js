ab1d68ca30557dbb2bd4b5bf68f9451a75d26285
AdityaK001 <121371373+AdityaK001@users.noreply.github.com>
2023-01-17 11:26:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const bodyparser =require("body-parser");
+const _ = require("lodash");
+var cors = require('cors');
+const port = process.env.PORT || 8080;
+const mongoose = require("mongoose");
+
+mongoose.set('strictQuery', false);
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
+app.use(cors());
+
+mongoose.connect('mongodb://127.0.0.1:27017/todolist',()=>{
+  console.log('mongoodb');
+});
+
+
+const contentschema = mongoose.Schema({content:String,status:String},{versionKey: false});
+
+var Note = mongoose.model("Note",contentschema,'notes');
+
+
+
+app.get('/', async (req,res)=>{ 
+  
+  var listitems=[];
+ 
+  
+  listitems= await Note.find();
+  res.send({list:listitems});
+  
+});
+
+app.post('/',(req,res)=>{
+  
+  const newnote = new Note(
+    {content:req.body.content,
+      status:"incomplete"});
+   newnote.save();
+});
+
+app.patch('/',async(req,res)=>{
+  changestatus = (req.body.status==='incomplete')? "complete":"incomplete";
+  listitems= await Note.find({_id:req.body._id});
+  
+  await Note.findOneAndUpdate({_id:req.body._id},{status:changestatus});
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
